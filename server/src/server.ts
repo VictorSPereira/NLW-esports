57393d148a620721d959b430586ec87ac3d045df
vitorpereira-01@hotmail.com <vitorpereira-01@hotmail.com>
2022-10-05 21:59:20 -0300

    continue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,23 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express()
+
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    return response.json([
+
+    ])
+})
+
+app.get('/games/:id/discord', (request, response) => {
+    return response.json([
+        
+    ])
+})
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css'
 import logoImg from './assets/nlw-esports-logo.svg'
+import {MagnifyingGlass} from 'phosphor-react'
 
 function App() {
 
@@ -53,10 +54,15 @@ function App() {
     </div>
 
     <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden mx-10'>
-      <div className='bg-[#2A2634] px-8 py-6'>
+      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+        <div>
         <strong className='text-2x1 text-white font-black block'>Não encontrou seu duo?</strong>
         <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players</span>
-
+        </div>
+      <div className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex gap-3 items-center'>
+        <MagnifyingGlass/>
+        Publicar anúncio
+      </div>
       </div>
     </div>
   </div>
